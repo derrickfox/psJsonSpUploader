fec705c0c4ce07919811205ffe6991434235e3e7
aaFox <aafoxdm2@nih.gov>
2019-07-09 11:32:44 -0400

    updating

--- test.ps1
+++ test.ps1
@@ -29,17 +29,11 @@ $json1 = Get-Content -Raw -Path $JsonFilePath | ConvertFrom-Json
 		{
 			Write-Host "Could not open List : " + $listName -ForegroundColor Red
 			return
-		}else{
-			Write-Output "theList is $theList"
 		}
 
 
-		# $JsonFilePath = 'C:\Users\aafoxdm2\Desktop\powerShellConverter\thing.json'
-		# $json1 = Get-Content -Raw -Path $JsonFilePath | ConvertFrom-Json
-		# Write-Output "Start"
-		# Write-Output $json1
-		# Write-Output "End"
-
+		$JsonFilePath = 'C:\Users\aafoxdm2\Desktop\powerShellConverter\thing.json'
+		$json1 = Get-Content -Raw -Path $JsonFilePath | ConvertFrom-Json
 		# $getLI = $json1.projects[0].LeadInvestigators
 		# $getLI = $getLI.split("(")
 		# $getLI = $getLI.split(" ")
@@ -50,48 +44,49 @@ $json1 = Get-Content -Raw -Path $JsonFilePath | ConvertFrom-Json
 		# $loginName = $sam.split("=")[1];
 		# $loginName = $loginName.split("}")[0];
 		# $reportTitle = $theWeb.ensureuser($loginName)
-		$reportTitle = $json1.projects[$num]["Title"]
-		$nihProjectID = $json1.projects[$num]["NIH Project ID"]
-		$ziaIdNumber = $json1.projects[$num]["ZIA ID Number"]
-		$ncatsDivision = $json1.projects[$num]["NCATS Division"]
-		$2018ProjectStatus = $json1.projects[$num]["2018 Project Status"]
-		$2019ProjectStatus = $json1.projects[$num]["2019 Project Status"]
-		$leadInvestagors = $json1.projects[$num]["Lead Investigators"]
-		$supervisorOrRecord = $json1.projects[$num]["Supervisor of Record"]
-		$ncatsTeamMembers = $json1.projects[$num]["NCATS Team Members"]
-		$intCollabs = $json1.projects[$num]["Intramural Collaborators (Affiliation)"]
-		$extCollabs = $json1.projects[$num]["Extramural Collaborators (Affiliation)"]
-		$humanCells = $json1.projects[$num]["Does project use human cells, human subjects or human tissues?"]
-		$keywords = $json1.projects[$num]["Keywords"]
-		$distinguishingKeyword = $json1.projects[$num]["Distinguishing Keyword"]
-		$goalsAndObjectives = $json1.projects[$num]["Goals and Objectives"]
-		$summary = $json1.projects[$num]["Summary"]
+		
+		$count = 1
+
+		$reportTitle = $json1[$count]."Report Title"
+		$nihProjectID = $json1[$count]."NIH Project ID"
+		$ziaIdNumber = $json1[$count]."ZIA ID Number"
+		$ncatsDivision = $json1[$count]."NCATS Division"
+		$2018ProjectStatus = $json1[$count]."2018 Project Status"
+		$2019ProjectStatus = $json1[$count]."2019 Project Status"
+		$leadInvestagors = $json1[$count]."Lead Investigators"
+		$supervisorOrRecord = $json1[$count]."Supervisor of Record"
+		$ncatsTeamMembers = $json1[$count]."NCATS Team Members"
+		$intCollabs = $json1[$count]."Intramural Collaborators (Affiliation)"
+		$extCollabs = $json1[$count]."Extramural Collaborators (Affiliation)"
+		$humanCells = $json1[$count]."Does project use human cells, human subjects or human tissues?"
+		$keywords = $json1[$count]."Keywords"
+		$distinguishingKeyword = $json1[$count]."Distinguishing Keyword"
+		$goalsAndObjectives = $json1[$count]."Goals and Objectives"
+		$summary = $json1[$count]."Summary"
 
 		if($reportTitle -ne $Null){
 			$newItem = $theList.items.add()
-			$newitem["Title"] = $reportTitle
+			$newitem["ReportTitle"] = $reportTitle
 			$newItem["NIH Project ID"] = $nihProjectID
 			$newItem["ZIA ID Number"] = $ziaIdNumber
 			$newItem["NCATS Division"] = $ncatsDivision
 			$newItem["2018 Project Status"] = $2018ProjectStatus
 			$newItem["2019 Project Status"] = $2019ProjectStatus
-			$newItem["Lead Investigators"] = $leadInvestagors
-			$newItem["Supervisor of Record"] = $supervisorOrRecord
-			$newItem["NCATS Team Members"] = $ncatsTeamMembers
+			# $newItem["Lead Investigators"] = $leadInvestagors
+			# $newItem["Supervisor of Record"] = $supervisorOrRecord
+			# $newItem["NCATS Team Members"] = $ncatsTeamMembers
 			$newItem["Intramural Collaborators (Affiliation)"] = $intCollabs
 			$newItem["Extramural Collaborators (Affiliation)"] = $extCollabs
-			$newItem["Does project use human cells, human subjects or human tissues?"] = $humanCells
+			$newItem["DoesProjectUseHumanBiospecimen"] = $humanCells
 			$newItem["Keywords"] = $keywords
 			$newItem["Distinguishing Keyword"] = $distinguishingKeyword
 			$newItem["Goals and Objectives"] = $goalsAndObjectives
 			$newItem["Summary"] = $summary
-			# $newitem.Update();
+			$newitem.Update();
 		}
+		$theWeb.Close()
+		$theWeb.Dispose()
 	}
-
-	# $theWeb.Close()
-	# $theWeb.Dispose()
-	
 
 	Function Update-Projects
 	{
@@ -100,15 +95,13 @@ $json1 = Get-Content -Raw -Path $JsonFilePath | ConvertFrom-Json
 
 	$theSite = New-Object Microsoft.SharePoint.SPSite($Url)
 
-	# Update-Projects
-	$count = 0
+	Update-Projects
+
+	# $count = 0
 	# foreach ($num in $json1) {
 	# 	# Update-Projects
-	# 	# Write-Output $num["Report Title"]
 	# 	$count++
-	# 	Write-Output $count
 	# }
-	Write-Output $json1[0]
 
 
     $theSite.Close()
